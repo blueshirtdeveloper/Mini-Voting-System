2a9de7b89555b3b58115f7ef2b59568ba4b7f3fb
Tummala Krishna kishore(Mac) <krishna30kishore@gmail.com>
2022-04-23 14:00:02 +0530

    changes

---
+++ voting_app/Shared/voting_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  voting_appApp.swift
+//  Shared
+//
+//  Created by Tummala Krishna kishore on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct voting_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
